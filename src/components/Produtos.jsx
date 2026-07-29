5342896ac8d0a884b99fb0549d22754e593654d5
frankwsb <frankwsb@uninove.br>
2026-07-29 16:15:12 -0300

    products component

---
+++ src/components/Produtos.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./Produtos.module.css"
+
+const Produtos = () => {
+  return (
+    <div className={styles.produto}>
+      <h2>Produtos</h2>
+    </div>
+  )
+}
+
+export default Produtos
