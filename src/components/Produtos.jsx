c8aa1d55b732062ed8cd03edb5c7220ec54891fd
frankwsb <frankwsb@uninove.br>
2026-07-29 17:42:04 -0300

    fetch products and styling component

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -1,11 +1,28 @@
 import React from "react"
 import styles from "./Produtos.module.css"
+import { Link } from "react-router-dom"
+import Head from "./Head"
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then(r => r.json())
+      .then(rJson => setProdutos(rJson))
+  }, [])
+
+  if(!produtos) return null
   return (
-    <div className={styles.produto}>
-      <h2>Produtos</h2>
-    </div>
+    <section className={styles.produtos + ' animeLeft'}>
+      <Head title={'Ranek | Produtos'} description="Produtos" />
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+          <h1 className={styles.nome}>{produto.nome}</h1>
+        </Link>
+      ))}
+    </section>
   )
 }
 
